d6b5ea2a31baca1e31c23e7eaa4279dfde415c8e
paul dalton <paul@p3d.co.uk>
2022-03-17 16:20:11 +0000

    Modify cassette and expectations
    To test following all internal links I have created a basic test website which means changing the cassette

--- spec/models/crawler_spec.rb
+++ spec/models/crawler_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe Crawler, type: :model do
   describe '#crawl' do
     it 'crawls a site and creates a Website entry' do
-      domain = 'p3d.co.uk'
+      domain = 'localhost:8000'
       VCR.use_cassette 'crawler' do
         expect{ Crawler.new(domain).crawl }.to change{ Website.count }.by(1)
         website = Website.find_by(domain: domain)
-        expect(website.num_internal_links).to eq 12
-        expect(website.num_external_links).to eq 2
+        expect(website.num_internal_links).to eq 4
+        expect(website.num_external_links).to eq 1
       end
     end
   end
